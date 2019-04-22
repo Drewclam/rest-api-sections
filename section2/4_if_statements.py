3a6ab7a66c9f25c885880972ac6ba10b1be230ca
Drewclam <andrew.lam@rangle.io>
2019-04-22 14:31:13 -0400

    Add solution

--- section2/4_if_statements.py
+++ section2/4_if_statements.py
@@ -17,15 +17,14 @@ if user_name in my_known_people:
 #### Exercise
 
 def who_do_you_know():
-    names = input("Enter the names of people you know, separated by commas: ")
-    names_list = names.split(",")
-    return names_list
+    people = input("Give me a list of people you know separated by commas: ")
+    return [person.strip() for person in people.split(',')]
 
 def ask_user():
-    # Ask user for their name
-    # See if their name is in list of people
-    # Print something if it is
+    person = input("Give me a name: ")
+    if person in who_do_you_know():
+        print('You know {}!'.format(person))
 
-    user_name = input("Enter your name: ")
-    if user_name in who_do_you_know():
-        print("Hello {}, I know you!".format(user_name))
+ask_user()
+
+# print([n for n in range(10) if n % 2 == 0])
