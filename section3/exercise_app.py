396882c04ea537d9f6725544f84a907e6bb5b377
Drewclam <andrew.lam@rangle.io>
2019-04-23 15:08:03 -0400

    Add render layer, add route logic

--- section3/exercise_app.py
+++ section3/exercise_app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
 stores = [
     {
-        'name': 'THe first store',
+        'name': 'My Wonderful Store',
         'items': [
             {
                 'name': 'Best item',
@@ -15,34 +15,63 @@ stores = [
 ]
 
 
+def createErrorMessage(message):
+    return {'message': message}
+
+
 @app.route('/')  # Home page of the site
 def home():
-    return 'Hello, world!'
+    return render_template('exercise_index.html')
 
 
 @app.route('/store')
 def getStores():
-    pass
+    return jsonify({'stores': stores})
 
 
 @app.route('/store/<string:name>')
-def getStore():
-    pass
+def getStore(name):
+    store = list(filter(lambda x: x['name'] == name, stores))
+    if len(store) > 0:
+        return jsonify({'store': store})
+    else:
+        return jsonify(createErrorMessage('No store found!'))
 
 
 @app.route('/store', methods=['POST'])
 def createStore():
-    pass
+    requestData = request.get_json()
+    newStore = {
+        'name': requestData['name'],
+        'items': []
+    }
+    stores.append(newStore)
+    return jsonify(newStore)
 
 
 @app.route('/store/<string:name>/item', methods=['POST'])
 def createItem(name):
-    pass
+    requestData = request.get_json()
+    store = list(filter(lambda x: x['name'] == name, stores))
+    item = {
+        'name': requestData['name'],
+        'price': requestData['price']
+    }
+    if len(store) > 0:
+        store[0]['items'].append(item)
+        return jsonify(item)
+    else:
+        return jsonify(createErrorMessage('Error: Item could not be created.'))
 
 
 @app.route('/store/<string:name>/item')
-def getItems():
-    pass
+def getItems(name):
+    store = list(filter(lambda store: store['name'] == name, stores))
+    if len(store) > 0:
+        items = store[0]['items']
+        return jsonify({'items': items})
+    else:
+        return jsonify(createErrorMessage('Error: Could not retrieve items for that store'))
 
 
 app.run(port=5000)
